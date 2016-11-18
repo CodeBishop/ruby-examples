9731897fbe3785021de3111a3ae5dd332200eb60
codebishop <sbishop@longdays.com>
2016-11-18 11:34:48 -0800

    Added more examples.

--- examples.rb
+++ examples.rb
@@ -3,10 +3,17 @@
 print 'The "print" statements don\'t do newlines'
 puts " but \"puts\" statements do."
 
+# Prints: Numbers 7 and 8.
 a, b = 7, 8
-print 'Numbers like ', a, " and #{b} can be concatenated with commas or embedded ", 'with #{ }\n'
+print "Numbers ", a, " and #{b}.\n"
 
-print "Semi-colons"
-def someFun; 2+3;end
+# Prints: String literals ignore #{} and \n embeddings.
+puts 'String literals ignore #{} and \n embeddings.'
 
+# Functions without arguments don't need ().
+def someFun
+  2+3
+end
+puts "someFun() returns #{someFun} because it's the last evaluated expression."
 
+print ""
