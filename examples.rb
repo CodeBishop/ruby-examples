e6b6f1a156a91a30aa3c7ce86016acd935eeeeef
codebishop <sbishop@longdays.com>
2016-11-18 11:13:28 -0800

    Initial commit

---
+++ examples.rb
@@ -0,0 +1,12 @@
+
+
+print 'The "print" statements don\'t do newlines'
+puts " but \"puts\" statements do."
+
+a, b = 7, 8
+print 'Numbers like ', a, " and #{b} can be concatenated with commas or embedded ", 'with #{ }\n'
+
+print "Semi-colons"
+def someFun; 2+3;end
+
+
